f394bb5ccd87383cc166c1e99af03135303071cb
Mahmoud Samy <mahmoudsamy@Mahmouds-MacBook-Pro.local>
2026-02-01 18:10:24 +0200

    fix bugs update

--- phone_webhook.py
+++ phone_webhook.py
@@ -1,5 +1,5 @@
 
-    from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import phonenumbers
 from phonenumbers import geocoder, carrier
